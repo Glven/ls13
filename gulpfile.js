6e6b86e97399aeb25fb92f8d42321997639bfa52
Tigran Oganesyan <t-oganesyan00@yandex.ru>
2020-05-06 00:48:46 +0300

    creating a server that tracks the change of files in the index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
 
-exports.default = defaultTask
+// Save a reference to the `reload` method
+
+// Watch scss AND html files, doing different things with each.
+gulp.task('serve', function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("*.html").on("change", reload);
+});
